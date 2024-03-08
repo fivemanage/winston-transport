f1634e51fde913998f00261974273f63967fcef5
c-wide <53365073+c-wide@users.noreply.github.com>
2024-03-08 14:59:41 -0600

    init

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import fetch from "node-fetch";
+import Transport from "winston-transport";
+import { getErrorMessage, setImmediateInterval } from "./misc";
+
+export type FivemanageTransportOptions = {
+	apiKey: string;
+	batchInterval?: number;
+	batchCount?: number;
+	shouldReprocessFailedBatches?: boolean;
+} & Transport.TransportStreamOptions;
+
+export class FivemanageTransport extends Transport {
+	private readonly apiUrl = "https://api.fivemanage.com/api/logs/batch";
+	private readonly apiKey: string;
+	private batch: Array<Record<string, unknown>> = [];
+	private readonly batchInterval: number;
+	private readonly batchCount: number;
+	private readonly shouldReprocessFailedBatches: boolean;
+
+	constructor(opts: FivemanageTransportOptions) {
+		super(opts);
+
+		this.apiKey = opts.apiKey;
+		this.batchInterval = opts.batchInterval ?? 5000;
+		this.batchCount = opts.batchCount ?? 10;
+		this.shouldReprocessFailedBatches =
+			opts.shouldReprocessFailedBatches ?? true;
+
+		this.startInterval();
+	}
+
+	startInterval() {
+		setImmediateInterval(async () => {
+			await this.processBatch();
+		}, this.batchInterval);
+	}
+
+	async processBatch() {
+		if (this.batch.length === 0) return;
+
+		const batch = this.batch;
+		this.batch = [];
+
+		try {
+			const res = await fetch(this.apiUrl, {
+				method: "POST",
+				body: JSON.stringify(batch),
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: this.apiKey,
+				},
+			});
+
+			if (res.ok === false) {
+				throw new Error("Failed to upload logs to Fivemanage");
+			}
+		} catch (error) {
+			console.error(`Failed to process log batch: ${getErrorMessage(error)}`);
+
+			if (this.shouldReprocessFailedBatches) {
+				this.batch.concat(batch);
+			}
+		}
+	}
+
+	log(info: Record<string, unknown>, next: () => void): void {
+		this.batch.push({
+			level: info.level,
+			message: info.message,
+			resource: info.resource,
+			metadata: info.metadata,
+		});
+
+		if (this.batch.length >= this.batchCount) {
+			this.processBatch();
+		}
+
+		next();
+	}
+}
---
+++ src/misc.ts
@@ -0,0 +1,19 @@
+export async function setImmediateInterval(cb: () => void, ms: number) {
+	cb();
+	return setInterval(cb, ms);
+}
+
+export function getErrorMessage(error: unknown) {
+	if (typeof error === "string") return error;
+
+	if (
+		error &&
+		typeof error === "object" &&
+		"message" in error &&
+		typeof error.message === "string"
+	) {
+		return error.message;
+	}
+
+	return "Unknown Error";
+}
